e27e94ef9a49846dd61c871a7c54780120763040
Joe Lanman <joelanman@gmail.com>
2017-06-01 14:34:23 +0100

    use a default search engine, accept array of values for 'source'

--- src/wrapper.js
+++ src/wrapper.js
@@ -1,19 +1,21 @@
 import { createElement, render } from 'preact' /** @jsx createElement */
 import Autocomplete from './autocomplete'
 
-function accessibleAutocomplete (opts) {
-  if (!opts.element) { throw new Error('element is not defined') }
-  if (!opts.id) { throw new Error('id is not defined') }
-  if (!opts.source) { throw new Error('source is not defined') }
-
-  render(<Autocomplete {...opts} />, opts.element)
-}
-
 const createSimpleEngine = results => (query, syncResults) => {
   const filteredResults = query
     ? results.filter(r => r.toLowerCase().indexOf(query.toLowerCase()) !== -1)
     : []
   syncResults(filteredResults)
+}
+
+function accessibleAutocomplete (opts) {
+  if (!opts.element) { throw new Error('element is not defined') }
+  if (!opts.id) { throw new Error('id is not defined') }
+  if (!opts.source) { throw new Error('source is not defined') }
+  if (Array.isArray(opts.source)) {
+    opts.source = createSimpleEngine(opts.source)
+  }
+  render(<Autocomplete {...opts} />, opts.element)
 }
 
 accessibleAutocomplete.enhanceSelectElement = (opts) => {
@@ -22,8 +24,7 @@ accessibleAutocomplete.enhanceSelectElement = (opts) => {
   // Set defaults.
   if (!opts.source) {
     let availableOptions = [].filter.call(opts.selectElement.options, o => (o.value || opts.preserveNullOptions))
-    availableOptions = availableOptions.map(o => o.innerHTML)
-    opts.source = createSimpleEngine(availableOptions)
+    opts.source = availableOptions.map(o => o.innerHTML)
   }
   opts.onConfirm = opts.onConfirm || (query => {
     const requestedOption = [].filter.call(opts.selectElement.options, o => o.innerHTML === query)[0]
